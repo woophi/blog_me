5051ea8266586ce4491325cd955362956f077661
Konstantin <konstantin.miheev@sensearena.com>
2023-08-31 13:58:55 +0200

    removed zoom animation

--- components/ui/cells/index-layout/BlogPreview.tsx
+++ components/ui/cells/index-layout/BlogPreview.tsx
@@ -1,6 +1,5 @@
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-import Zoom from '@material-ui/core/Zoom';
 import { goToSpecific } from 'core/common';
 import { BlogGuestItem } from 'core/models';
 import moment from 'moment';
@@ -10,7 +9,6 @@ type Props = BlogGuestItem;
 
 export const BlogPreview = React.memo<Props>(
   ({ blogId, coverPhotoUrl, publishedDate, title, views }) => {
-    
     const loadBlog = React.useCallback(() => {
       const mapTitle = title.toLowerCase().split(' ').join('-');
       goToSpecific(`/post/${mapTitle}-${blogId}`);
@@ -22,47 +20,43 @@ export const BlogPreview = React.memo<Props>(
       'h_400/' +
       coverPhotoUrl.substr(indexOfUpload);
     return (
-      <Zoom in mountOnEnter timeout={1000}>
-        <Box
-          minWidth="320px"
-          style={{
-            cursor: 'pointer',
-          }}
-          onClick={loadBlog}
-          display="flex"
-          flexDirection="column"
-        >
-          <Box width="100%" height="100%" maxHeight="505px">
-            <picture>
-              <source
-                type="image/webp"
-                data-srcset={coverPhotoThumbnail + '.webp'}
-              />
-              <img
-                alt={title}
-                data-src={coverPhotoThumbnail + '.jpg'}
-                style={{
-                  width: '100%',
-                  height: '100%',
-                  objectFit: 'cover',
-                }}
-                className="lazyload"
-              />
-            </picture>
-          </Box>
-          <Box width="100%" padding="1rem" display="flex" flexDirection="column">
-            <Typography variant="h5" component="h1" title={title} gutterBottom>
-              {title}
-            </Typography>
-            <Typography variant="caption" display="block" gutterBottom>
-              Опубликовано: {moment(publishedDate).fromNow()}
-            </Typography>
-            <Typography variant="caption" display="block" gutterBottom>
-              Прочтений: {views}
-            </Typography>
-          </Box>
+      <Box
+        minWidth="320px"
+        style={{
+          cursor: 'pointer',
+        }}
+        onClick={loadBlog}
+        display="flex"
+        flexDirection="column"
+      >
+        <Box width="100%" height="100%" maxHeight="505px">
+          <picture>
+            <source type="image/webp" data-srcset={coverPhotoThumbnail + '.webp'} />
+            <img
+              alt={title}
+              data-src={coverPhotoThumbnail + '.jpg'}
+              style={{
+                width: '100%',
+                height: '100%',
+                objectFit: 'cover',
+              }}
+              className="lazyload"
+            />
+          </picture>
         </Box>
-      </Zoom>
+        <Box width="100%" padding="1rem" display="flex" flexDirection="column">
+          <Typography variant="h5" component="h1" title={title} gutterBottom>
+            {title}
+          </Typography>
+          <Typography variant="caption" display="block" gutterBottom>
+            Опубликовано: {moment(publishedDate).fromNow()}
+          </Typography>
+          <Typography variant="caption" display="block" gutterBottom>
+            Прочтений: {views}
+          </Typography>
+        </Box>
+      </Box>
     );
   }
 );
+
